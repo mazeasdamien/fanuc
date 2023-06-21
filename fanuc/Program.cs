3ac807aea1b0bf4ac6508aaa0e0e6dc587d448e5
Damien Mazeas <58029218+mazeasdamien@users.noreply.github.com>
2023-06-21 11:22:54 +0100

    Update Program.cs

--- fanuc/Program.cs
+++ fanuc/Program.cs
@@ -60,10 +60,6 @@ namespace FanucRobotServer
                 xyzWpr.P = 62.596;
                 xyzWpr.R = -180;
                 */
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
                 //chatGPT
                 
                 xyzWpr.X = 1200;
@@ -308,28 +304,16 @@ namespace FanucRobotServer
                             xyzWpr.Z = 835.273;
                             xyzWpr.W = 0;
                             xyzWpr.P = 62.596;
-<<<<<<< Updated upstream
                             xyzWpr.R = -180;*/
 
                             //chatGPT
-
-=======
-                            xyzWpr.R = -180;
-                            */
-                            //chatGPT
-                            
->>>>>>> Stashed changes
                             xyzWpr.X = 1200;
                             xyzWpr.Y = 50;
                             xyzWpr.Z = 900;
                             xyzWpr.W = 0;
                             xyzWpr.P = 0;
                             xyzWpr.R = 90;
-<<<<<<< Updated upstream
 
-=======
-                            
->>>>>>> Stashed changes
                             Thread.Sleep(500);
                             sysGroupPosition.Update();
                             Thread.Sleep(500);
